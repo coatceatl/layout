0074b257f3498fda96ce5349ba46b889dca866c3
Oksana Tkachenko <oksana@crystax.net>
2018-04-12 16:42:54 +0300

    WIP, changed jquery, css for mobile menu

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   $('#nav-toggle').on('click', function(e) {
     e.preventDefault();
-    $('#navigation').addClass('nav-menu-1-mobile');
+    $('#navigation').addClass('nav-menu-mobile');
+    $('#menu').addClass('nav-menu-mobile');
+/*    $('#contact').addClass('nav-menu-mobile');*/
+
   });
 });

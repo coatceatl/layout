8066b7edd34b560c7a1290c66f0cdaece7204b00
Oksana Tkachenko <oksana@crystax.net>
2018-04-14 23:19:43 +0300

    adds transform hamburger to cross

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#nav-toggle').on('click', function(e) {
+  $('.hamburger').on('click', function(e) {
     e.preventDefault();
     $(this).toggleClass('active');
     $('#header').toggleClass('nav-menu-mobile');
